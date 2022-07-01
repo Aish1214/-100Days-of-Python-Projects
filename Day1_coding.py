11c6fbaeece09a77df60ddb666f760f90301d8db
Aish1214 <funaguskaraishwarya@gmail.com>
2022-07-02 00:22:38 +0600

    FIRST COMMIT

---
+++ Day1_coding.py
@@ -0,0 +1,20 @@
+print("Hello world")
+
+# use for comment which is completely ignored by the compiler. It is used so that when we open the code after long time we come to understand
+#what is being written in the python code.
+print('Hello' + input('What is your name') + '!')
+
+# printing length of the name entered
+print(len(input('What is your name? ')))
+
+#printing variables
+name = input("What is your name? ")
+print(len(name))
+
+#switching the entered value
+a = input('a : ')
+b = input('b: ')
+print(a)
+print(b)
+new = print("a: " + b)
+good = print("b: " + a)
---
+++ bandnamegenerator.py
@@ -0,0 +1,10 @@
+#to print the name 
+print("Welcome to the Band Name Generator")
+#Asking the name of the city they grew up
+a = input("What's the name of the city you grew up in? ")
+print(a)
+#Asking for the pet's name
+b = input("What's your pet's name? ")
+print(b)
+#Concatenation of city's name  +  Pet's name
+print("Your band name could be" + " " + a  + " " +  b )
